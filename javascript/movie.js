1a83b32c40841de520cd4f9b8135b078698a3e51
SNGCHN LEE <sngchn0312@naver.com>
2024-04-29 16:31:49 +0900

    comment: 주석 추가

--- javascript/movie.js
+++ javascript/movie.js
@@ -6,7 +6,7 @@ const options = {
   },
 };
 
-// 영화 데이터를 저장하는 변수
+// 영화 데이터를 저장하는 배열 변수 선언
 let movies = [];
 
 // 타이틀을 눌렀을 시, 페이지 리로드
@@ -15,6 +15,8 @@ document.getElementById('title').addEventListener('click', function () {
 });
 
 // 영화 정보 불러오기
+// async await 비동기, fetch 함수로 API 통신
+// try catch를 사용해 에러를 핸들링함
 async function fetchMovies() {
   try {
     const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko-KR', options);
@@ -44,10 +46,9 @@ function displayMovies(movies) {
   });
 }
 
-// 자동으로 검색에 커서 위치 시키기
 document.addEventListener('DOMContentLoaded', () => {
-  fetchMovies(); // 영화 데이터 불러오기
-  document.getElementById('search-input').focus();
+  fetchMovies();
+  document.getElementById('search-input').focus(); // 자동으로 검색에 커서 위치 시키기
 });
 
 document.getElementById('search-btn').addEventListener('click', function (event) {
@@ -56,7 +57,7 @@ document.getElementById('search-btn').addEventListener('click', function (event)
   searchMovies(keyword);
 });
 
-//.toLowerCase로 대소문자 구분없이 검색
+// 위에서 받은 keyword를 .toLowerCase를 통해 대소문자 구분없이 검색
 function searchMovies(keyword) {
   const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(keyword.toLowerCase()));
   displayMovies(filteredMovies);
